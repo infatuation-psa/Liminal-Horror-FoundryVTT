47a357605adfaf55fdea8dc3bb0d4b5e461e61e6
Alec MacDonald <alec@psarecords.com>
2023-01-29 16:12:33 -0500

    fix: Cairn -> liminal horror, update docs

--- module/character-generator.js
+++ module/character-generator.js
@@ -1,35 +1,35 @@
-import { CairnActor } from "./actor/actor.js";
+import { LiminalActor } from "./actor/actor.js";
 import {
   compendiumInfoFromString, drawTableItem,
   drawTableText, findCompendiumItem,
 } from './compendium.js'
-import { Cairn } from "./config.js";
+import { Liminal } from "./config.js";
 import { evaluateFormula, formatString } from './utils.js'
 
 /**
- * @returns {Promise.<CairnActor>}
+ * @returns {Promise.<LiminalActor>}
  */
 export const createCharacter = async () => createActorWithCharacter(await generateCharacter());
 
 /**
- * @param {CairnActor} actor
- @returns {Promise.<CairnActor>}
+ * @param {LiminalActor} actor
+ @returns {Promise.<LiminalActor>}
  */
 export const regenerateActor = async (actor)  => updateActorWithCharacter(actor, await generateCharacter());
 
 /**
  * @param {Object} characterData
- * @returns {Promise.<CairnActor>}
+ * @returns {Promise.<LiminalActor>}
  */
 export const createActorWithCharacter = async (characterData) => {
   const data = characterToActorData(characterData);
-  return CairnActor.create(data);
+  return LiminalActor.create(data);
 };
 
 /**
- * @param {CairnActor} actor
+ * @param {LiminalActor} actor
  * @param {Object} characterData
- @returns {Promise.<CairnActor>}
+ @returns {Promise.<LiminalActor>}
  */
 export const updateActorWithCharacter = async (actor, characterData) => {
   const data = characterToActorData(characterData);
@@ -62,7 +62,7 @@ export const rollTextItems = async (items) => {
 
 /**
  * @param {Object} items
- @return {Promise<CairnItem[]>}
+ @return {Promise<LiminalItem[]>}
  */
 export const rollItems = async (items) => {
   const result = [];
@@ -124,13 +124,13 @@ export const rollBiography = async (config) => formatString(config.text,{
 
 /**
  * @param {Object} items
- * @returns {Promise.<CairnItem[]>}
+ * @returns {Promise.<LiminalItem[]>}
  */
 export const rollStartingGear = async (items) => rollItems(items);
 
 /**
  * @param {Object} items
- * @return {Promise<CairnItem[]>}
+ * @return {Promise<LiminalItem[]>}
  */
 export const findStartingItems = async (items) => {
   const result = [];
@@ -152,7 +152,7 @@ export const findStartingItems = async (items) => {
 export const generateCharacter = async () => {
   console.log(`Creating new character`);
 
-  const characterGenerator = Cairn.characterGenerator;
+  const characterGenerator = Liminal.characterGenerator;
 
   const abilities = await rollAbilities(characterGenerator.ability);
   const hp = await rollHitProtection(characterGenerator.hitProtection);
--- module/item/item.js
+++ module/item/item.js
@@ -2,7 +2,7 @@
  * Extend the basic Item with some very simple modifications.
  * @extends {Item}
  */
-export class CairnItem extends Item {
+export class LiminalItem extends Item {
 	/**
 	 * Augment the basic item data with additional dynamic data.
 	 */
